39fe33665d91f7b14ae946e87455abfd6aa110d1
Martin Beierling <mbeierling@gmail.com>
2018-09-04 01:03:26 +0200

    Add react-spring

--- components/ui/Code.js
+++ components/ui/Code.js
@@ -1,20 +1,25 @@
-import Prism from "prismjs";
+import Prism from "prismjs"; // all we need to do is import it once
+import { Spring } from "react-spring";
 import Head from "next/head";
 
 export default ({ children }) => (
-  <pre>
-    {children}
-    <Head>
-      <link href="/static/prism.css" rel="stylesheet" />
-    </Head>
-    <style jsx>{`
-      pre {
-        position: relative;
-        top: 67px;
-        left: 85px;
-        margin-right: 170px;
-        transform: rotate(-4.4deg);
-      }
-    `}</style>
-  </pre>
+  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+    {styles => (
+      <pre style={styles}>
+        {children}
+        <Head>
+          <link href="/static/prism.css" rel="stylesheet" />
+        </Head>
+        <style jsx>{`
+          pre {
+            position: relative;
+            top: 67px;
+            left: 85px;
+            margin-right: 170px;
+            transform: rotate(-4.4deg);
+          }
+        `}</style>
+      </pre>
+    )}
+  </Spring>
 );
