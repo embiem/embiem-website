b05ffd4fabf705c339f37c95700fd2b8165d5225
Martin Beierling-Mutz <mbeierling@gmail.com>
2018-09-21 17:35:04 +0200

    Update to next.js 7.0 & fix code highlighting issue on client

--- components/ui/Code.js
+++ components/ui/Code.js
@@ -1,25 +1,34 @@
 import Prism from "prismjs"; // all we need to do is import it once
+import { Component } from "react";
 import { Spring } from "react-spring";
 import Head from "next/head";
 
-export default ({ children }) => (
-  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-    {styles => (
-      <pre style={styles}>
-        {children}
-        <Head>
-          <link href="/static/prism.css" rel="stylesheet" />
-        </Head>
-        <style jsx>{`
-          pre {
-            position: relative;
-            top: 67px;
-            left: 85px;
-            margin-right: 170px;
-            transform: rotate(-4.4deg);
-          }
-        `}</style>
-      </pre>
-    )}
-  </Spring>
-);
+export default class extends Component {
+  componentDidMount() {
+    // make sure we highlight code components on the client
+    window.Prism.highlightAll();
+  }
+  render() {
+    return (
+      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {styles => (
+          <pre style={styles}>
+            {this.props.children}
+            <Head>
+              <link href="/static/prism.css" rel="stylesheet" />
+            </Head>
+            <style jsx>{`
+              pre {
+                position: relative;
+                top: 67px;
+                left: 85px;
+                margin-right: 170px;
+                transform: rotate(-4.4deg);
+              }
+            `}</style>
+          </pre>
+        )}
+      </Spring>
+    );
+  }
+}
