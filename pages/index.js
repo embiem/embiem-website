bca384e2028f89762bb0810ef024428c3dbc37a0
Martin Beierling <mbeierling@gmail.com>
2018-09-26 23:41:44 +0200

    Finalize desktop profile page

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,15 @@ a:nth-of-type(2) {
   left: -66px;
   top: 120px;
   transform: rotate(-0.2deg);
-}`;
+}
+
+a:nth-of-type(3) {
+  position: relative;
+  left: -161px;
+  top: 170px;
+  transform: rotate(1.6deg);
+}
+`;
 
 export default class extends React.Component {
   static async getInitialProps() {
@@ -66,6 +74,7 @@ export default class extends React.Component {
               text="GitHub"
             />
             <IconLink
+              href="https://gitlab.com/embiem"
               className={buttonClassName}
               iconSrc="static/Gitlab_font_awesome.svg"
               text="GitLab"
@@ -82,20 +91,30 @@ export default class extends React.Component {
           />
           <div>
             <IconLink
+              href="https://www.linkedin.com/in/mbeierling/"
               className={buttonClassName}
               iconSrc="static/iconmonstr-linkedin-3.svg"
               text="Linkedin"
             />
             <IconLink
+              href="https://twitter.com/em_bi_em"
               className={buttonClassName}
               iconSrc="static/Twitter_Logo_Blue.svg"
               text="Twitter"
             />
+            <IconLink
+              href="https://keybase.io/mbeierling"
+              className={buttonClassName}
+              iconSrc="static/keybase-icon.svg"
+              text="Keybase"
+            />
           </div>
           <hr />
           <h2>Let's connect!</h2>
-          <img src="static/martin-wedding.jpg" alt="Me at my wedding" />
-          <caption>That's me at my wedding.</caption>
+          <div className="profile-image">
+            <img src="static/martin-wedding.jpg" alt="Me at my wedding" />
+            <span>That's me at my wedding.</span>
+          </div>
           <style jsx>{`
             .profile {
               width: 577px;
@@ -107,16 +126,25 @@ export default class extends React.Component {
             }
 
             h2 {
-              position: relative;
-              left: 165px;
-              top: 50px;
-              transform: rotate(2.1deg);
-
               font-size: 14px;
               font-weight: 400;
             }
 
-            hr {
+            h2:nth-of-type(1) {
+              position: relative;
+              left: 165px;
+              top: 50px;
+              transform: rotate(2.1deg);
+            }
+            h2:nth-of-type(2) {
+              font-weight: bold;
+              position: relative;
+              left: 205px;
+              top: 50px;
+              transform: rotate(-1.1deg);
+            }
+
+            hr:nth-of-type(1) {
               width: 120px;
               transform: rotate(82deg);
               position: relative;
@@ -124,14 +152,41 @@ export default class extends React.Component {
               top: 67px;
             }
 
-            img {
+            hr:nth-of-type(2) {
+              width: 155px;
+              transform: rotate(82deg);
+              position: relative;
+              left: -110px;
+              top: 86px;
+            }
+
+            .profile-image {
               position: relative;
               left: 450px;
               top: -10px;
+              display: inline-block;
+            }
+
+            img {
               box-shadow: rgba(0, 0, 0, 0.25) 4px 4px 4px;
               border-radius: 47px;
               width: 180px;
               transform: rotate(3.5deg);
+            }
+
+            span {
+              opacity: 0;
+              color: ${colors.primary};
+              position: relative;
+              left: -172px;
+              top: 20px;
+              font-weight: 500;
+              font-size: 14px;
+              transition: opacity 0.2s;
+            }
+
+            .profile-image:hover span {
+              opacity: 1;
             }
           `}</style>
           {buttonStyles}
