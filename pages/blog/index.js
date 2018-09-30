574ad559712c0a1463bafc8ebfee8370a2990ffe
Martin Beierling <mbeierling@gmail.com>
2018-09-30 15:44:56 +0200

    Re-organize blog's index page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,54 @@
-import Blog from "../blog";
+import Link from "next/link";
+import faker from "faker";
+import parse from "date-fns/parse";
+
+import Layout from "../../components/Layout";
+import PostPreview from "../../components/blog/PostPreview";
+
 import { meta as metaTestTwo } from "./test-two.mdx";
 import { meta as metaTest } from "./test.mdx";
 
-export const posts = [metaTestTwo, metaTest];
+const posts = [metaTestTwo, metaTest];
 
-export default () => <Blog />;
+posts.sort(function(a, b) {
+  // Turn your strings into dates, and then subtract them
+  // to get a value that is either negative, positive, or zero.
+  return new Date(b.date) - new Date(a.date);
+});
+
+export default class extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div className="spacer" />
+        <div className="blog-posts">
+          {posts.map(post => (
+            <React.Fragment>
+              <Link href={post.link}>
+                <a>
+                  <PostPreview {...post} />
+                </a>
+              </Link>
+              <div className="spacer" />
+            </React.Fragment>
+          ))}
+        </div>
+        <style jsx>{`
+          .spacer {
+            height: 55px;
+          }
+          .blog-posts {
+            display: flex;
+            flex-direction: column;
+            max-width: 600px;
+          }
+
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+        `}</style>
+      </Layout>
+    );
+  }
+}
