9eb780637f8944d63e9b546647504a6249916bff
Martin Beierling-Mutz <mbeierling@gmail.com>
2018-10-09 00:04:50 +0200

    Refactor recursive getPathsObject function

--- scripts/getPathsObject.js
+++ scripts/getPathsObject.js
@@ -1,32 +1,36 @@
 const fs = require("fs");
 
 module.exports = () => {
-  const walkSync = function(dir, filelist) {
+  const fileObj = {};
+
+  const walkSync = dir => {
+    // Get all files of the current directory & iterate over them
     const files = fs.readdirSync(dir);
-    filelist = filelist || [];
-    files.forEach(function(file) {
-      if (fs.statSync(dir + file).isDirectory()) {
-        filelist = walkSync(dir + file + "/", filelist);
+    files.forEach(file => {
+      // Construct whole file-path & retrieve file's stats
+      const filePath = `${dir}${file}`;
+      const fileStat = fs.statSync(filePath);
+
+      if (fileStat.isDirectory()) {
+        // Recurse one folder deeper
+        walkSync(`${filePath}/`);
       } else {
-        filelist.push(dir + file);
+        // Construct this file's pathname excluding the "pages" folder & its extension
+        const cleanFileName = filePath
+          .substr(0, filePath.lastIndexOf("."))
+          .replace("pages/", "");
+
+        // Add this file to `fileObj`
+        fileObj[`/${cleanFileName}`] = {
+          page: `/${cleanFileName}`,
+          lastModified: fileStat.mtime
+        };
       }
     });
-    return filelist;
   };
 
-  let files = [];
-  walkSync("pages/", files);
-
-  const fileObj = {};
-  files.forEach(file => {
-    const fileStat = fs.statSync(file) || {};
-    const fileExtIdx = file.lastIndexOf(".");
-    const cleanFileName = file.substr(0, fileExtIdx).replace("pages/", "");
-    fileObj[`/${cleanFileName}`] = {
-      page: `/${cleanFileName}`,
-      lastModified: fileStat.mtime
-    };
-  });
+  // Start recursion to fill `fileObj`
+  walkSync("pages/");
 
   return fileObj;
 };
