c7f067c30e9a13b32f17a29015b51b573a228a27
Martin Beierling-Mutz <mbeierling@gmail.com>
2018-12-08 02:09:34 +0100

    Add more redirects & improve <ul> styling

--- components/ui/UnorderedList.js
+++ components/ui/UnorderedList.js
@@ -3,10 +3,13 @@ import { MediaQueryConsumer } from "../../utils/withMediaQuery";
 export default ({ children }) => (
   <MediaQueryConsumer>
     {({ isMobile, isTablet }) => (
-      <ul>
+      <ul className="dynamic-shadow">
         {children}
         <style jsx>{`
           ul {
+            font-size: ${isMobile ? "16px" : "18px"};
+            letter-spacing: 0.7px;
+            line-height: 34px;
             padding: 0 ${isMobile ? "40px" : "64px"};
             transform: rotate(${!isMobile && !isTablet ? "-2.4deg" : "-0.4deg"});
             list-style-image: url("/static/images/squirrel.png");
