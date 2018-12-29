3578949d045baf29ba45f4328a9e408ca65d0a62
Martin Beierling-Mutz <mbeierling@gmail.com>
2018-12-29 10:31:09 +0100

    Put buttons under posts in withMDX util

--- utils/withMdx.js
+++ utils/withMdx.js
@@ -4,6 +4,8 @@ import uiComponents from "../components/ui";
 import Layout from "../components/Layout";
 import PaperPage from "../components/blog/PaperPage";
 import SocialMeta from "../components/SocialMeta";
+import IconLink from "../components/interaction/IconLink";
+import PostSection from "../components/blog/PostSection";
 
 /* const Comments = dynamic(() => import("../components/blog/Comments"), {
   ssr: false
@@ -17,6 +19,28 @@ export default meta => ({ children }) => (
         <content>
           <uiComponents.h1>{meta.title}</uiComponents.h1>
           {children}
+          <PostSection>
+            <IconLink
+              href="https://www.linkedin.com/in/mbeierling/"
+              iconSrc="/static/iconmonstr-linkedin-3.svg"
+              text="Linkedin"
+            />
+            <IconLink
+              href="https://twitter.com/em_bi_em"
+              iconSrc="/static/Twitter_Logo_Blue.svg"
+              text="Twitter"
+            />
+            <IconLink
+              href="https://dev.to/embiem"
+              iconSrc="/static/dev-badge.svg"
+              text="DEV.to"
+            />
+            <IconLink
+              href="https://keybase.io/mbeierling"
+              iconSrc="/static/keybase-icon.svg"
+              text="Keybase"
+            />
+          </PostSection>
         </content>
       </PaperPage>
     </Layout>
